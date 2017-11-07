fde160265b96b64894b98ffd20822efbdfaf7399
Robert Fux <robfox170@gmail.com>
2017-11-07 21:02:45 +0100

    CrossProduct() Vector Operator

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -23,7 +23,7 @@ void ATankAIController::Tick(float DeltaTime)
 
 	ControlledTank->AimAt(PlayerTank->GetActorLocation());
 
-	ControlledTank->Fire();
+	//ControlledTank->Fire();
 	
 }
 
--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -29,11 +29,18 @@ void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool
 	// No need to call Super:: as we are replacing the functionality here
 	auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
 	auto AIForwardIntention = MoveVelocity.GetSafeNormal();
-	// Using dot product (scalar product) to obtain a forward throw based on a cosine curve (-1 to 1) representing the parallelness
-	// of the tank forward and the AI forward intention vectors
+
+	// Using vector dot product (projection or scalar product) to obtain a forward throw based on a cosine curve (1 0 -1 0) 
+	// representing the parallelism of the tank forward and the AI forward intention vectors
 	float ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention);
 	IntendMoveForward(ForwardThrow);
-	UE_LOG(LogTemp, Warning, TEXT("Throw is %f "), ForwardThrow)
+
+	// Using vector cross product to obtain a right throw based on a sine curve (0 1 0 -1) 
+	// representing the perpendicularity of the tank forward and the AI forward intention vectors. 
+	// Note that order in the operation matters, because the result is oriented along the Z axis
+	auto RightThrow = FVector::CrossProduct(TankForward, AIForwardIntention).Z;
+	IntendTurnRight(RightThrow);
+	//UE_LOG(LogTemp, Warning, TEXT("Throw is %f "), RightThrow)
 }
 
 
